6f0207e2dcd05d5089ea8facc730a4be818dc610
Rickiarty <fatalframe0719@gmail.com>
2019-05-02 16:06:23 +0800

    Add files via upload

---
+++ Calculus/DefiniteIntegral/definite_integral.py
@@ -0,0 +1,24 @@
+
+def eval_single_interval(math_func, from_x, to_x):
+    if from_x > to_x:
+        t = from_x
+        from_x = to_x
+        to_x = t
+    dx = to_x - from_x
+    from_y = math_func(from_x)
+    to_y = math_func(to_x)
+    aver_y = (from_y + to_y) / 2.0
+    return aver_y * dx
+
+def summarize_multiple_integrals(math_func, from_x, to_x, div_num):
+    coef = 1
+    sum = 0.0
+    if from_x > to_x:
+        t = from_x
+        from_x = to_x
+        to_x = t
+        coef = -1
+    dx = (float(to_x) - float(from_x)) / div_num
+    for i in range(div_num):
+        sum += eval_single_interval(math_func, float(from_x) + i*dx, float(from_x) + ((i+1)*dx))
+    return coef * sum
---
+++ Calculus/main.py
@@ -0,0 +1,14 @@
+import sys
+from DefiniteIntegral.definite_integral import summarize_multiple_integrals
+
+def example_math_func1(x):
+    x = float(x)
+    y = 3 * (x**2)
+    return float(y)
+
+from_x = float(sys.argv[1])
+to_x = float(sys.argv[2])
+div_num = int(sys.argv[3])
+
+sum = summarize_multiple_integrals(example_math_func1, from_x, to_x, div_num)
+print(str(sum))
