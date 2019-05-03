c1730f2d3845971d2d78663ecaeb8e190f0bdcaa
Rickiarty <fatalframe0719@gmail.com>
2019-05-03 23:27:32 +0800

    Update definite_integral.py

--- Calculus/DefiniteIntegral/definite_integral.py
+++ Calculus/DefiniteIntegral/definite_integral.py
@@ -4,7 +4,7 @@
  * 
 """
 
-def eval_single_interval(math_func, from_x, to_x):
+def eval_single_partition(math_func, from_x, to_x):
     if from_x > to_x:
         t = from_x
         from_x = to_x
@@ -15,7 +15,7 @@ def eval_single_interval(math_func, from_x, to_x):
     aver_y = (from_y + to_y) / 2.0
     return aver_y * dx
 
-def summarize_multiple_integrals(math_func, from_x, to_x, div_num):
+def summarize_multiple_partitions(math_func, from_x, to_x, div_num):
     coef = 1
     sum = 0.0
     if from_x > to_x:
@@ -25,5 +25,5 @@ def summarize_multiple_integrals(math_func, from_x, to_x, div_num):
         coef = -1
     dx = (float(to_x) - float(from_x)) / div_num
     for i in range(div_num):
-        sum += eval_single_interval(math_func, float(from_x) + i*dx, float(from_x) + ((i+1)*dx))
+        sum += eval_single_partition(math_func, float(from_x) + i*dx, float(from_x) + ((i+1)*dx))
     return coef * sum
